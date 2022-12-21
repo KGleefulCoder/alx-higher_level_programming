91969839b81f96345189e6b4f4920a0b02cbac15
KGleefulCoder <khadijah6360@gmail.com>
2022-12-21 13:23:59 +0100

    Tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,4 @@
-u#!/usr/bin/python3
+#!/usr/bin/python3
 """Square module.
 This module contains a class that defines a square and its size and its
 position on the screen, checking if the given values are right, and a setter
